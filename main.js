649de82579c593f3c46f809e8f955292edea1d41
SUNNATBEE <yusupovsunnatbek32@gmail.com>
2025-03-07 11:15:45 +0500

    Proekt boshlandi

---
+++ main.js
@@ -0,0 +1,28 @@
+const box = document.querySelector("#list")
+
+function renderUser(url){
+    box.innerHTML = "";
+    fetch(url)
+    .then(lavash => lavash.json())
+    .then(lavshKeldi => {
+        
+        lavshKeldi.map(obyektlar => {
+            box.innerHTML += `
+            <li class="w-[300px] bg-green-500 rounded p-5 flex flex-col items-center">
+            <h2>${obyektlar.name}</h2>
+        <p>Email: ${obyektlar.address.street}</p>
+            </li>
+            
+            `
+        })
+    })
+    .catch(xato => {
+        console.log("Gamburger keldi");
+        
+    })
+    .finally( ()=> {
+        console.log("Ogohlantirish");
+        
+    })
+}
+renderUser("https://jsonplaceholder.typicode.com/users")
